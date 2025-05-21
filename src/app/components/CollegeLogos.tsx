bd4546d91c3d889494e5e6d56109d9370a3eed53
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2025-05-21 12:14:19 -0700

    added more logos

--- src/app/components/CollegeLogos.tsx
+++ src/app/components/CollegeLogos.tsx
@@ -1,34 +1,37 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import Aos from "aos";
 
 const collegeLogos = [
   { image: "/logos/cal.png", alt: "Cal" },
   { image: "/logos/ucla.svg", alt: "UCLA" },
   { image: "/logos/uci.svg", alt: "UCI" },
   { image: "/logos/yale.svg", alt: "Yale" },
-  { image: "/logos/cal.png", alt: "Cal" },
-  { image: "/logos/ucla.svg", alt: "UCLA" },
-  { image: "/logos/uci.svg", alt: "UCI" },
-  { image: "/logos/yale.svg", alt: "Yale" },
+  { image: "/logos/neu.png", alt: "NEU" },
+  { image: "/logos/jhu.svg", alt: "JHU" },
+  { image: "/logos/um.svg", alt: "UMich" },
 ];
 
 export const CollegeLogos = () => {
+    useEffect (() => {
+    Aos.init({ duration: 500, once: true}); // Added once: true to ensure animation only happens on first scroll
+  }, [])
   return (
     <div className="w-full bg-white py-4 md:py-7 overflow-hidden">
       <ul className="flex w-max min-w-full shrink-0 flex-nowrap gap-16 py-2">
-        <motion.div
+        <div
           className="align-center flex w-full justify-center overflow-hidden gap-20"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
         >
           {collegeLogos.map((logo, index) => {
             return (
               <li
                 className="relative h-[60px] w-[120px] max-w-full flex-shrink-0 "
                 key={index}
+                data-aos="fade-left" // Added AOS animation type
+                data-aos-delay={index * 50} // Added cascading delay
+                data-aos-offset="-500"
               >
                 {" "}
                 <Image
@@ -41,7 +44,7 @@ export const CollegeLogos = () => {
               </li>
             );
           })}
-        </motion.div>
+        </div>
       </ul>
     </div>
   );
